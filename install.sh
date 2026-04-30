ff3937f99d00f52114efa7bef6e53c67ec9ed732
Mig <miguelphpro@gmail.com>
2026-04-30 19:35:07 -0300

    Installer

--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-sudo curl -L https://raw.githubusercontent.com/codexaaa/dns-test-mig/main/dns-test-mig.py -o /usr/bin/dns-test-mig
 
-sudo chmod +x /usr/bin/dns-test-mig
+if [ -d "$PREFIX/bin" ]; then
+    # Configuração para Termux
+    DESTINO="$PREFIX/bin/dns-test-mig"
+    CMD_INSTALL="pip install questionary"
+    SUDO=""
+else
+    DESTINO="/usr/local/bin/dns-test-mig"
+    CMD_INSTALL="pip3 install questionary --break-system-packages"
+    SUDO="sudo"
+fi
+
+echo "Installing dependencies..."
+$CMD_INSTALL
+
+echo "Downloading DNS Test by Mig..."
+$SUDO curl -L https://raw.githubusercontent.com/codexaaa/dns-test-mig/main/dns-test-mig.py -o $DESTINO
+
+echo "Setting permissions..."
+$SUDO chmod +x $DESTINO
 
 echo "----------------------------------------------"
 echo "Installation Complete!"
