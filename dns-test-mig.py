1c56b04d47c50befd42a517396062f1250f0752b
Mig <miguelphpro@gmail.com>
2026-04-30 19:38:28 -0300

    Principal Archive

--- dns-test-mig.py
+++ dns-test-mig.py
@@ -5,14 +5,36 @@ import questionary
 
 # List of DNS providers to test
 DNS_PROVIDERS = {
-    "Google": "8.8.8.8",
-    "Cloudflare": "1.1.1.1",
-    "OpenDNS": "208.67.222.222",
-    "Quad9": "9.9.9.9",
-    "AdGuard": "94.140.14.14",
-    "DNS.Watch": "84.200.69.80",
-    "Comodo Secure DNS": "8.26.56.26",
-    "OpenDNS": "208.67.222.222",
+    "Google 1": "8.8.8.8",
+    "Google 2": "8.8.4.4",
+    "Cloudflare 1": "1.1.1.1",
+    "Cloudflare 2": "1.0.0.1",
+    "OpenDNS (Cisco)": "208.67.222.222",
+    "Quad9 (Secure)": "9.9.9.9",
+    "GigaDNS 1": "189.38.95.95",
+    "GigaDNS 2": "189.38.95.96",
+    "Umbler (Brazil)": "187.84.150.123",
+    "Algar Telecom": "201.48.24.11",
+    "Vivo (Brazil)": "200.204.0.10",
+    "Claro/NET (Brazil)": "201.21.192.101",
+    "AdGuard (Block Ads)": "94.140.14.14",
+    "Mullvad (Privacy)": "194.242.2.2",
+    "Control D": "76.76.2.0",
+    "CleanBrowsing (Family)": "185.228.168.168",
+    "DNS.WATCH": "84.200.69.80",
+    "LibreDNS": "116.202.176.26",
+    "Comodo Secure": "8.26.56.26",
+    "Level3 (Lumen)": "4.2.2.1",
+    "Verisign": "64.6.64.6",
+    "Oracle (Dyn)": "216.146.35.35",
+    "Freenom": "80.80.80.80",
+    "Yandex (Russia)": "77.88.8.8",
+    "Tencent (China)": "119.29.29.29",
+    "AliDNS (Alibaba)": "223.5.5.5",
+    "OpenNIC (Global)": "185.121.177.177",
+    "Neustar": "156.154.70.1",
+    "Hurricane Electric": "74.82.42.42"
+}
     
     
 }
