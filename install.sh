5bc1b193112e2a9011588b963c59b4006d143ab2
Mig <miguelphpro@gmail.com>
2026-04-30 13:54:32 -0300

    Installer

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# Baixa o código do seu GitHub
 sudo curl -L https://raw.githubusercontent.com/codexaaa/dns-test-mig/main/dns-test-mig.py -o /usr/bin/dns-test-mig
 
-# DÁ A PERMISSÃO DE EXECUÇÃO (Isso é o que está faltando!)
 sudo chmod +x /usr/bin/dns-test-mig
 
 echo "----------------------------------------------"
